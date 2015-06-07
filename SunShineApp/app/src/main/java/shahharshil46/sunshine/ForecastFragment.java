cc7d464a4a9ea5c392285a8aaa35339d68af275c
shahharshil46 <shahharshil46@gmail.com>
2015-06-07 18:18:53 +0530

    converting between metric and imperial

--- SunShineApp/app/src/main/java/shahharshil46/sunshine/ForecastFragment.java
+++ SunShineApp/app/src/main/java/shahharshil46/sunshine/ForecastFragment.java
@@ -2,10 +2,10 @@ package shahharshil46.sunshine;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
@@ -33,7 +33,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 
 /**
@@ -124,24 +123,31 @@ public class ForecastFragment extends Fragment {
         String userLocation = PreferenceManager
                 .getDefaultSharedPreferences(getActivity())
                 .getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        String temperatureUnit = PreferenceManager
+                .getDefaultSharedPreferences(getActivity())
+                .getString(getString(R.string.pref_temperature_units_key), getString(R.string.pref_temperature_units_metric));
         Log.d(LOG_TAG,"****** User location is "+userLocation);
-        fetchWeatherTask.execute(userLocation);
+        Log.d(LOG_TAG,"****** Temperature Unit is "+temperatureUnit);
+
+        fetchWeatherTask.execute(userLocation, temperatureUnit);
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]>{
 
         public final String LOG_TAG = FetchWeatherTask.class.getName();
         String postalCode = "";
+        String temperatureUnit = "";
         @Override
         protected String[] doInBackground(String... strings) {
-            if(strings.length>0)
+            if(strings.length>0) {
                 postalCode = strings[0];
+            }
             else{
-                postalCode="94043";
+                postalCode= getString(R.string.pref_location_default);
             }
             String format = "json";
             int noOfDays = 7;
-            String units = "metrics";
+            String units = getString(R.string.pref_temperature_units_metric);
             try {
 
                 final String baseURL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
@@ -237,12 +243,20 @@ public class ForecastFragment extends Fragment {
     /**
      * Prepare the weather high/lows for presentation.
      */
-    private String formatHighLows(double high, double low) {
+    private String formatHighLows(double high, double low, String unitType) {
+
+        if (unitType.equals(getString(R.string.pref_temperature_units_imperial))) {
+            high = (high * 1.8) + 32;
+            low = (low * 1.8) + 32;
+        } else if (!unitType.equals(getString(R.string.pref_temperature_units_metric))) {
+            Log.d(LOG_TAG, "Unit type not found: " + unitType);
+        }
+
         // For presentation, assume the user doesn't care about tenths of a degree.
         long roundedHigh = Math.round(high);
         long roundedLow = Math.round(low);
 
-        String highLowStr = roundedHigh + "/" + roundedLow;
+        String highLowStr = roundedHigh + " / " + roundedLow;
         return highLowStr;
     }
 
@@ -285,6 +299,13 @@ public class ForecastFragment extends Fragment {
         dayTime = new Time();
 
         String[] resultStrs = new String[numDays];
+
+        SharedPreferences sharedPrefs =
+                PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        String unitType = sharedPrefs.getString(
+                getString(R.string.pref_temperature_units_key),
+                getString(R.string.pref_temperature_units_metric));
         for(int i = 0; i < weatherArray.length(); i++) {
             // For now, using the format "Day, description, hi/low"
             String day;
@@ -312,7 +333,7 @@ public class ForecastFragment extends Fragment {
             double high = temperatureObject.getDouble(OWM_MAX);
             double low = temperatureObject.getDouble(OWM_MIN);
 
-            highAndLow = formatHighLows(high, low);
+            highAndLow = formatHighLows(high, low, unitType);
             resultStrs[i] = day + " - " + description + " - " + highAndLow;
         }
 
